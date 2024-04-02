17f9d267ef36c6db3559e7662f6ebd203fc8975b
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-04-03 02:57:14 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,26 @@
+// import React, { useState } from "react";
+// import ShoppingList from "./ShoppingList";
+// import Header from "./Header";
+// import itemData from "../data/items";
+
+// function App() {
+//   const [items, setItems] = useState(itemData);
+//   const [isDarkMode, setIsDarkMode] = useState(false);
+
+//   function handleDarkModeClick() {
+//     setIsDarkMode((isDarkMode) => !isDarkMode);
+//   }
+
+//   return (
+//     <div className={"App " + (isDarkMode ? "dark" : "light")}>
+//       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+//       <ShoppingList items={items} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
@@ -14,7 +37,7 @@ function App() {
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} setItems={setItems} />
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,25 +1,82 @@
+// import React, { useState } from "react";
+// import ItemForm from "./ItemForm";
+// import Filter from "./Filter";
+// import Item from "./Item";
+
+// function ShoppingList({ items }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All");
+
+//   function handleCategoryChange(event) {
+//     setSelectedCategory(event.target.value);
+//   }
+
+//   const itemsToDisplay = items.filter((item) => {
+//     if (selectedCategory === "All") return true;
+
+//     return item.category === selectedCategory;
+//   });
+
+//   return (
+//     <div className="ShoppingList">
+//       <ItemForm />
+//       <Filter onCategoryChange={handleCategoryChange} />
+//       <ul className="Items">
+//         {itemsToDisplay.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
+
+
 import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items , setItems}) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchItem, setSearchItem] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
+ function  handleSearchChange(e) {
+    setSearchItem(e.target.value);
+  }
+
+  
+
+
+
+  const itemsToDisplay = items.filter((item) => {
+    if (searchItem.length == 0 && selectedCategory === "All") {
+      return true;
+    } 
+    else if (item.category === selectedCategory && searchItem.length == 0){
+      return item.category === selectedCategory;
+    }
+     else if (searchItem.length > 0 && selectedCategory === "All") {
+      return item.name.toLowerCase().includes(searchItem.toLocaleLowerCase());
+    }
+     else if(item.category === selectedCategory && searchItem.length > 0){
+      return item.name.toLowerCase().includes(searchItem.toLocaleLowerCase()) && item.category === selectedCategory;
+    } 
   });
+
+  const onItemFormSubmit = (obj) => {
+    setItems([...items, obj]);
+  };
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} search={searchItem} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
